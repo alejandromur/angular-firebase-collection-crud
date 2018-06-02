ace40af09cd09e418ffe508c7ff001cbad759324
alejandro mur <alejandro.mur@boream.com>
2018-06-02 19:33:31 +0200

    header, footer, main components created

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'collection-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+
+}
